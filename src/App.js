7acc098314b8a744301b85043e9728c502e28ea9
Shohel <922229@cognizant.com>
2021-12-29 10:25:09 +0530

    changing themes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import NavBar from './Components/Navbar';
 import Body from './Components/Body';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <br />
-      <br />
       <Body />
     </div>
   );
--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,12 +1,28 @@
-import React from "react";
-import "../Styles/pageStyle.scss";
+import React from 'react';
+import NavBar from './Navbar';
+import '../Styles/pageStyle.scss';
+import { useState } from 'react/cjs/react.development';
 export default function Body() {
+  const [Theme, setCurrentTheme] = useState('default');
+  const getTheme = (Theme) => {
+    setCurrentTheme(Theme);
+    // console.log(Theme);
+    // let elemname = document.querySelector(`.background`);
+    // console.log(elemname);
+  };
   return (
-    <div className="background">
-      <div className="firstBorder">
-        <div className="secondBorder">
-          <div className="thirdBorder">
-            <div className="innerCircle"></div>
+    <div>
+      <div className={`background background_theme_${Theme}`}>
+        <br />
+        <br />
+        <NavBar onGetTheme={getTheme} />
+        <br />
+        <br />
+        <div className={`firstBorder firstBorder_theme_${Theme}`}>
+          <div className={`secondBorder secondBorder_theme_${Theme}`}>
+            <div className={`thirdBorder thirdBorder_theme_${Theme}`}>
+              <div className="innerCircle"></div>
+            </div>
           </div>
         </div>
       </div>
